82f81b9871c58e5d71f28b62bce3a4980afe1b92
Jamie Lopez <jmelopez@outlook.com>
2020-09-10 18:59:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function nowServing(katzDeliLine) {
     return ('There is nobody waiting to be served!')
 }
 }
+
+function currentLine(katzDeliLine) {
+  var array = []
+  if(katzDeliLine.length > 0) {
+    for(var i=0; i < katzDeliLine.length; i++) {
+    array += (i+1) + '. ' + katzDeliLine[i] + ', ';
+  }
+  array = array.slice(0, array.length -2);
+  return 'The line is currently: ' + array;
+}else{
+  return 'The line is currently empty.'
+}
+}
