d209c8b38d0e787762cb30cb027399bfde8b267b
Jamie Lopez <jmelopez@outlook.com>
2020-09-10 17:44:37 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function takeANumber(name,number) {
+  for(var i=0; i<number.length; i++){
+    katzDeliLine.push(`Welcome, ${name[i]}. You are ${number[i]} in line.`)
+  }
+  return katzDeliLine;
+  }
+  
